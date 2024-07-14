9d529cf30c13fbc5c9713381b85e70f793c862cf
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-07-13 21:55:17 -0300

    :white_check_mark:  feat: finished arrays

--- 05_PHP/27_array.php
+++ 05_PHP/27_array.php
@@ -40,3 +40,49 @@ foreach ($values as $value) {
   echo $value;
   echo "<br>";
 }
+
+// Array Slice
+$array_slice = [
+  "a", "b", "c", "d", "e", "f", "g"
+];
+
+$slice = array_slice($array_slice, 1, 5);
+
+print_r($slice);
+
+echo "<br>";
+
+// Array Splice
+$array_splice = [
+  "a", "b", "c", "d", "e", "f", "g"
+];
+
+$splice = array_splice($array_splice, 1, 2, "x");
+
+print_r($splice);
+
+echo "<br>";
+
+// Array Reduce
+$array_reduce = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+$reduce = array_reduce($array_reduce, function ($acc, $item) {
+  return $acc + $item;
+});
+
+echo $reduce;
+
+echo "<br>";
+
+// Descontraido
+$joao = [
+  "nome" => "João",
+  "idade" => 19,
+  "cidade" => "New York",
+  "cor" => "Preto",
+  "profissao" => "Desenvolvedor"
+];
+
+list("nome" => $nome, "idade" => $idade, "cidade" => $cidade, "cor" => $cor, "profissao" => $profissao) = $joao;
+
+echo "Nome: $nome, Idade: $idade, Cidade: $cidade, Cor: $cor, Profissão: $profissao";
